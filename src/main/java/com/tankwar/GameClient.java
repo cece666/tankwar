6e742f75088a4c7d62599ffd937ccc61ed84980e
tumc <deutschlandcece666@gmail.com>
2021-01-11 15:25:21 +0100

    tank+icon

---
+++ src/main/java/com/tankwar/GameClient.java
@@ -0,0 +1,34 @@
+package com.tankwar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent {
+
+
+    public GameClient() {
+        //800*600 window:
+        this.setPreferredSize(new Dimension(800,600));
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(), 400, 100, null);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("CC 的坦克大战");
+        frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
+        GameClient client = new GameClient();
+        frame.add(client);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+
+    }
+}
