d73860c1210b0259b764abef9dd6ac7ad79ba249
tumc <deutschlandcece666@gmail.com>
2021-01-14 14:46:14 +0100

    add shooting sound

--- src/main/java/com/tankwar/Tank.java
+++ src/main/java/com/tankwar/Tank.java
@@ -1,8 +1,11 @@
 package com.tankwar;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 //用来控制它的坐标，从而达到移动的效果
 public class Tank {
@@ -162,6 +165,10 @@ public class Tank {
         Missile missile = new Missile(x + getImage().getWidth(null) / 2 - 6,
                 y + getImage().getHeight(null) / 2 - 6, enemy, direction);
         GameClient.getInstance().getMissiles().add(missile);
+
+        Media sound = new Media(new File("assets/audios/shoot.wav").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
     }
 
     boolean stopped;
